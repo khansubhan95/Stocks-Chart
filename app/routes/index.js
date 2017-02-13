a2b4ba769f4d0804249f61b12300ce7a6afda545
subhan <khansubhan20@yahoo.com>
2017-02-13 20:46:37 +0530

    Add routes for post data

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,4 +8,24 @@ module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.render('list')
 	})
+
+	app.post('/', urlencodedParser, function(req, res) {
+		var symbol = req.body.symbol.toUpperCase()
+		console.log(symbol);
+		var APIEndPoint = 'https://www.quandl.com/api/v3/datasets/WIKI/' + symbol.toString() + '.json?api_key=' + process.env.QUANDL_KEY
+		console.log(APIEndPoint);
+		request.get(APIEndPoint, function(err, response, body) {
+			if (!err && response.statusCode===200) {
+				body = JSON.parse(body)
+				console.log(typeof body);
+				var data = {}
+				data.id = body.dataset.id
+				data.symbol = body.dataset.dataset_code
+				data.name = body.dataset.name
+				data.start_date = body.dataset.start_date
+				data.end_date = body.dataset.end_date
+				res.send(body.dataset.id)
+			}
+		})
+	})
 }
