16b79fe243e82fcfbc2d8955f267c009a3d3163b
subhan <khansubhan20@yahoo.com>
2017-02-14 21:29:08 +0530

    Add static files

--- app/models/companies.js
+++ app/models/companies.js
@@ -3,10 +3,6 @@ var mongoose = require('mongoose')
 var companySchema = new mongoose.Schema({
 	id: String,
 	symbol: String,
-	name: String,
-	start_date: String,
-	end_date: String,
-	stocks: {type: Array, "default": []}
 })
 
 module.exports = companySchema
